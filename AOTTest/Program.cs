d4334c7ade2bfcefe2bff42825ee1cf46dc81d26
Michael Yuen <michael@overimagined.com>
2024-08-07 15:54:59 +0800

    Use function pointer

--- AOTTest/Program.cs
+++ AOTTest/Program.cs
@@ -4,18 +4,32 @@ using System.Runtime.InteropServices;
 namespace AOTTest
 {
 
-    internal class Program
+    unsafe internal class Program
     {
+        //[DllImport("Wrapper.dll")]
+        //public static extern void CreateSimulationInstance();
+        //[DllImport("Wrapper.dll")]
+        //public static extern void StepSimulation();
+        //[DllImport("Wrapper.dll")]
+        //public static extern Vector3 GetBodyPos();
 
-        [DllImport("Wrapper.dll")]
-        public static extern void CreateSimulationInstance();
-        [DllImport("Wrapper.dll")]
-        public static extern void StepSimulation();
-        [DllImport("Wrapper.dll")]
-        public static extern Vector3 GetBodyPos();
+        static delegate* unmanaged[Cdecl]<void> CreateSimulationInstance;
+        static delegate* unmanaged[Cdecl]<void> StepSimulation;
+        static delegate* unmanaged[Cdecl]<Vector3> GetBodyPos;
+
 
         static void Main(string[] args)
         {
+            var handle = NativeLibrary.Load("Wrapper.dll");
+            var CreateSimulationInstancePtr = NativeLibrary.GetExport(handle, "CreateSimulationInstance");
+            var StepSimulationPtr = NativeLibrary.GetExport(handle, "StepSimulation");
+            var GetBodyPosPtr = NativeLibrary.GetExport(handle, "GetBodyPos");
+
+
+            CreateSimulationInstance = (delegate* unmanaged[Cdecl]<void>)CreateSimulationInstancePtr;
+            StepSimulation = (delegate* unmanaged[Cdecl]<void>)StepSimulationPtr;
+            GetBodyPos = (delegate* unmanaged[Cdecl]<Vector3>)GetBodyPosPtr;
+
             CreateSimulationInstance();
 
             while (true)
