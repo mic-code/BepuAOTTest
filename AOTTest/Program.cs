b7e38686514afb33bc836d4bc34789cd9853f60c
Michael Yuen <michael@overimagined.com>
2024-08-26 11:21:47 +0800

    Added method to load function pointer

--- AOTTest/Program.cs
+++ AOTTest/Program.cs
@@ -13,9 +13,9 @@ namespace AOTTest
         //[DllImport("Wrapper.dll")]
         //public static extern Vector3 GetBodyPos();
 
-        static delegate* unmanaged[Cdecl]<void> CreateSimulationInstance;
-        static delegate* unmanaged[Cdecl]<void> StepSimulation;
-        static delegate* unmanaged[Cdecl]<Vector3> GetBodyPos;
+        static delegate* unmanaged<void> CreateSimulationInstance;
+        static delegate* unmanaged<void> StepSimulation;
+        static delegate* unmanaged<Vector3> GetBodyPos;
 
 
         static void Main(string[] args)
@@ -26,9 +26,13 @@ namespace AOTTest
             var GetBodyPosPtr = NativeLibrary.GetExport(handle, "GetBodyPos");
 
 
-            CreateSimulationInstance = (delegate* unmanaged[Cdecl]<void>)CreateSimulationInstancePtr;
-            StepSimulation = (delegate* unmanaged[Cdecl]<void>)StepSimulationPtr;
-            GetBodyPos = (delegate* unmanaged[Cdecl]<Vector3>)GetBodyPosPtr;
+            //CreateSimulationInstance = (delegate* unmanaged<void>)CreateSimulationInstancePtr;
+            //StepSimulation = (delegate* unmanaged<void>)StepSimulationPtr;
+            //GetBodyPos = (delegate* unmanaged<Vector3>)GetBodyPosPtr;
+
+            CreateSimulationInstance = GetPointer(CreateSimulationInstancePtr);
+            StepSimulation = GetPointer(StepSimulationPtr);
+            GetBodyPos = GetPointer<Vector3>(GetBodyPosPtr);
 
             CreateSimulationInstance();
 
@@ -38,6 +42,16 @@ namespace AOTTest
                 GetBodyPos();
                 Thread.Sleep(10);
             }
+        }
+
+        static delegate* unmanaged<void> GetPointer(nint p)
+        {
+            return (delegate* unmanaged<void>)p;
+        }
+
+        static delegate* unmanaged<T> GetPointer<T>(nint p)
+        {
+            return (delegate* unmanaged<T>)p;
         }
     }
 }
